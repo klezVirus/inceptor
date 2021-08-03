392d2cf3d2470b68f7dfdb84ddf77abdcdd3064c
d3adc0de <klez.virus@gmail.com>
2021-08-03 10:11:37 +0100

    Updated default config

--- inceptor/config/Config.py
+++ inceptor/config/Config.py
@@ -9,6 +9,7 @@ from utils.utils import get_project_root
 class Config(object):
     def __init__(self, auto_load: bool = True, create_env: bool = True):
         self.config = configparser.ConfigParser(allow_no_value=True, interpolation=configparser.ExtendedInterpolation())
+        self.default_config = os.path.join(get_project_root(), "config", "default.ini")
         self.file = os.path.join(get_project_root(), "config", "config.ini")
         if not os.path.isfile(self.file):
             self.write_default()
@@ -112,85 +113,9 @@ class Config(object):
         print(self.get("PLACEHOLDERS", "SHELLCODE"))
 
     def write_default(self):
+        content = open(self.default_config).read()
         with open(self.file, "w") as default:
-            default.write(r"""[COMPILERS]
-vcvarsall = 
-clx86_compiler = 
-clx64_compiler = 
-masmx86_compiler = 
-masmx64_compiler = 
-cscx86_compiler = C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
-cscx64_compiler = C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe
-clangx86_compiler =
-clangx64_compiler =
-llvmx86_compiler = 
-llvmx64_compiler = 
-msbuildx86_compiler =
-msbuildx64_compiler =
-libx64_compiler = 
-libx86_compiler = 
-
-[SIGNERS]
-signtool_x86 = 
-signtool_x64 = 
-
-[DUMPERS]
-dumpbin_x86 = 
-dumpbin_x64 = 
-
-[DIRECTORIES]
-artifacts = artifacts
-templates = templates\${MISC:release}
-bypass = ${TEMPLATES}\amsi
-antidebug = nodebug
-powershell = ${TEMPLATES}\powershell
-writer = temp
-certificates = certs
-native = ${TEMPLATES}\cpp
-dotnet = ${TEMPLATES}\csharp
-test = ${TEMPLATES}\testers
-dll = ${TEMPLATES}\cpp\code_execution
-obfuscators = obfuscators
-syscalls = syscalls
-syscalls_x86 = syscalls\syswhispersv2_x86\x86
-encoders = encoders\implementations\${MISC:release}
-libs = libs\public
-modules = engine\modules
-
-[OBFUSCATORS]
-powershell = ${DIRECTORIES:obfuscators}\powershell
-dotnet = ${DIRECTORIES:obfuscators}\dotnet
-native = ${DIRECTORIES:obfuscators}\native
-
-[SIGNING]
-domain = www.microsoft.com
-
-[PLACEHOLDERS]
-shellcode = ####SHELLCODE####
-code = //####CODE####
-call = //####CALL####
-using = //####USING####
-define = //####DEFINE####
-bypass = //####BYPASS####
-antidebug = //####ANTIDEBUG####
-unhook = //####UNHOOK####
-args = //####ARGS####
-delay = //####DELAY####
-find_process = //####FIND_PROCESS####
-shellcode_variable = encoded
-
-[SYSCALLS]
-syswhispers = 2
-
-[MISC]
-logo = 3
-bypass_mode = 100
-release = public
-
-[DEBUG]
-compilers = 0
-syswhispers = 0
-""")
+            default.write(content)
 
 
 if __name__ == "__main__":
