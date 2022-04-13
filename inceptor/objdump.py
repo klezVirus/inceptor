9543d074530881979243efe674069a1cdf7df818
d3adc0de <klez.virus@gmail.com>
2022-04-13 17:50:15 +0100

    Minor upgrade to shellcode utils

--- inceptor/objdump.py
+++ inceptor/objdump.py
@@ -15,6 +15,7 @@ class ObjectDumper:
         self.start = start
 
     def objdump(self, file):
+        chunks = []
         path = Path(file)
         if not path.is_file():
             raise FileNotFoundError
@@ -23,10 +24,11 @@ class ObjectDumper:
         md = Cs(CS_ARCH_X86, self.arch)
         for i in md.disasm(code, self.start):
             # print([a for a in i.__dir__() if not a.startswith("__")])
+            chunks.append(i.bytes)
             line = binascii.hexlify(i.bytes).decode()
             human = " ".join([line[i:i+2] for i in range(0, len(line), 2)])
             print(f"0x{i.address:08X}  {human:32} {i.mnemonic} {i.op_str}")
-
+        return chunks
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="objdump: Windows tool to dump object into ASM", add_help=True)
--- inceptor/shellcode_tester.py
+++ inceptor/shellcode_tester.py
@@ -1,8 +1,10 @@
 import argparse
 import os
 import subprocess
-from ctypes import c_int, pointer, windll
+from ctypes import c_int, pointer, windll, c_uint64, create_string_buffer
 
+import objdump
+from enums.Architectures import Arch
 from utils.utils import get_project_root, is_os_64bit
 
 PAGE_EXECUTE_READWRITE = 0x00000040
@@ -10,37 +12,70 @@ PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
 VIRTUAL_MEM = (0x1000 | 0x2000)
 kernel32 = windll.kernel32
 
+SPLIT_TEST = b"\xeb\x01\x90\x90\x90"  # JMP SHORT 0x3; nop; nop; nop
 
-def execute(shellcode):
+
+def execute(shellcode, arch=Arch.x64):
     print(f"[+] Executing the shellcode:{type(shellcode[0]).__name__} in a new thread")
     input("[*] Press a key to continue...")
     shellcode = bytes(shellcode)
     print(shellcode)
+    if arch == Arch.x64:
+        kernel32.VirtualAlloc.restype = c_uint64
     ptr = kernel32.VirtualAlloc(0, len(shellcode), VIRTUAL_MEM, PAGE_EXECUTE_READWRITE)
-    kernel32.RtlMoveMemory(
-        c_int(ptr),
-        shellcode,
-        c_int(len(shellcode))
-    )
-    ht = kernel32.CreateThread(c_int(0), c_int(0), c_int(ptr), c_int(0), c_int(0), pointer(c_int(0)))
-    kernel32.WaitForSingleObject(c_int(ht), c_int(-1))
+    if arch == Arch.x64:
+        kernel32.RtlMoveMemory(
+            c_uint64(ptr),
+            create_string_buffer(shellcode),
+            len(shellcode)
+        )
+        ht = kernel32.CreateThread(0, 0, c_uint64(ptr), 0, 0, 0)
+        kernel32.WaitForSingleObject(ht, -1)
+    else:
+        kernel32.RtlMoveMemory(
+            c_int(ptr),
+            shellcode,
+            c_int(len(shellcode))
+        )
+        ht = kernel32.CreateThread(c_int(0), c_int(0), c_int(ptr), c_int(0), c_int(0), pointer(c_int(0)))
+        kernel32.WaitForSingleObject(c_int(ht), c_int(-1))
+
+
+def split(file, split_len, arch=Arch.x64):
+    dumper = objdump.ObjectDumper(arch=arch)
+
+    shellcode = bytearray()
+    chunks = dumper.objdump(file)
+    length = 0
+    for c in chunks:
+        length += len(c)
+        if length >= split_len:
+            shellcode += SPLIT_TEST
+            length = 0
+        shellcode += c
+    return bytes(shellcode)
 
 
 def main(args):
-    with open(args.get("file"), "rb") as shf:
-        shellcode = shf.read()
+    file = args.get("file")
+    if args.get("split") > 0:
+        shellcode = split(file, args.get("split"))
+    else:
+        with open(file, "rb") as shf:
+            shellcode = shf.read()
+    arch = Arch.from_string(args.get("arch"))
     if args.get("debug"):
         shellcode = b"\xcc" + shellcode
-        debug()
-        execute(shellcode)
+        debug(arch=arch)
+    execute(shellcode, arch=arch)
 
 
-def debug(autorun=True, args=None, arch="x64"):
+def debug(autorun=True, args=None, arch=Arch.x64):
     print("[+] Opening WinDbg to test the shellcode")
     pid = os.getpid()
     custom_wew = get_project_root().joinpath("workspaces", "custom.wew")
 
-    cmd = f"\"C:\\Program Files{' (x86)' if is_os_64bit() else ''}\\Windows Kits\\10\\Debuggers\\{arch}\\windbg.exe\" " \
+    cmd = f"\"C:\\Program Files{' (x86)' if is_os_64bit() else ''}\\Windows Kits\\10\\Debuggers\\{arch.value}\\windbg.exe\" " \
           f"-WF \"{custom_wew}\" -p {pid} -c \".load pykd.pyd; {';'.join(args) + ';' if args else ''} g;\""
     print(cmd)
     if autorun:
@@ -63,6 +98,8 @@ def parse_args():
                         help="Add int3 to the shellcode and debug with WinDbg")
     parser.add_argument("-i", "--inject", action='store_true', required=False,
                         help="Add int3 to the shellcode and debug with WinDbg")
+    parser.add_argument("-s", "--split", type=int, required=False, default=0,
+                        help="Try to split shellcode in chunks of minimum X bytes")
     args = parser.parse_args()
     return vars(args)
 
