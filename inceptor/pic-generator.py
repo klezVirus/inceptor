89ccaa4bb2dc6fca22b91ef8f27e9b08e652fcfe
d3adc0de <klez.virus@gmail.com>
2022-06-10 08:19:59 +0100

    Add variable name support to PIC generator

--- inceptor/pic-generator.py
+++ inceptor/pic-generator.py
@@ -6,9 +6,10 @@ from converters.TextExtract import TextExtract
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Text Extractor Test")
     parser.add_argument("target", help="C file to compile")
+    parser.add_argument("-v", "--variable", required=False, default="shellcode", help="Variable name")
     args = parser.parse_args()
 
     t = TextExtract()
     raw = t.transform(target=args.target)
-    print(utils.utils.format_shellcode(raw))
+    print(utils.utils.format_shellcode(raw, variable=args.variable))
 
